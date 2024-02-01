0cbb4b8ef55fd5d1ec1e0082a83dcc9d8b588b09
papavansai <papavansai@gmail.com>
2024-02-01 11:49:37 +0530

    11:49

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { compileFactoryFunction } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { LayerGroup, Map, TileLayer } from 'leaflet';
+
+//import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'osm_stackblitz';
+
+  // map: Map = new Map('map',{}); //Map('element')
+  // markers: LayerGroup= new LayerGroup();
+  // baseLayer: TileLayer = new TileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png',{
+  //   maxZoom: 19,
+  //   attribution: '&copy'
+  // });
+
+  map : Map | any;
+  markers: LayerGroup | any;
+  baseLayer: TileLayer | any;
+
+  constructor(){
+    // this.map.setView([17.4065,78.4772],13);
+    // this.markers.addTo(this.map);
+    // this.baseLayer.addTo(this.map);
+  }
+  
+  ngOnInit(): void {
+    this.map = new Map('map',{}).setView([17.4065,78.4772],13);
+    this.markers = new LayerGroup().addTo(this.map);
+    this.baseLayer = new TileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png',{
+         maxZoom: 19,
+         attribution: '&copy'
+       }).addTo(this.map);
+
+  //   let map = L.map('map',{}).setView([17.4065, 78.4772], 13);
+  //   var markers = L.layerGroup().addTo(map);
+
+  //   L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //     maxZoom: 19,
+  //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  // }).addTo(map);
+  
+  }
+
 }
